b6457a310b38bf42b69a2e84393eadd98d41953c
catisioo <74970648+catisioo@users.noreply.github.com>
2024-03-13 09:27:12 +0200

    changed from tkinter to pygame

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-import tkinter as tk
+import pygame
 from functions import *
 
+
 # Test the function
-print(generate_numbers())
+# print(generate_numbers())
 
-#gui
-
-root = tk.Tk()
-root.geometry("1000x500")
-root.title("Spēle, komanda 34")
-
-autori = tk.Label(root, text="Iļja Poļivko,\n Mārtiņš Ņikiforovs,\n Raitis Bērtulis,\n Haralds Popovs,\nDaniela Smelte,\nViktors Krasnoborodjko")
-autori.pack(padx=20, pady=20)
-
-root.mainloop()
+#pygame gui
