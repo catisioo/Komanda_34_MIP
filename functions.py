f33b2a3bfe84997ba0963e0862efe234dd61e8fe
catisioo <74970648+catisioo@users.noreply.github.com>
2024-03-15 19:28:36 +0200

    GameNodeData + beginning of tree

--- functions.py
+++ functions.py
@@ -1,5 +1,8 @@
 import random
 
+# funkcija, kas ģenerē 5 skaitļus
+# atgriež ARRAY ar 5 skaitļiem
+
 def generate_numbers():
     numbers = []
     num = 20004
@@ -7,3 +10,28 @@ def generate_numbers():
         numbers.append(num)
         num += 12
     return random.sample(numbers, 5)
+
+# klase GameNode
+# satur skaitli, kas rodas dalot, punktu skaitu, ko iegūst spēlētājs
+# un heiristiko novērtējumu 
+
+class GameNodeData:
+    def __init__(self, number, points, value):
+        self.number = number
+        self.points = points
+        self.value = value
+
+    def __str__(self):
+        return f"number = {self.number}\npoints = {self.points}\nvalue = {self.value}"
+
+# ternary tree
+    
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.middle = None
+        self.right = None
+        self.data = data
+
+    def PrintTree(self):
+        print(self.data)
--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from functions import *
 
 # Test the function
 print(generate_numbers())
+
+# test
+root = Node(GameNodeData(27516, 0, 4))
+root.PrintTree()
