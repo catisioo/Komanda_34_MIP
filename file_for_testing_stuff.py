97924fe40a27f4a641d557918d3173478268e355
catisioo <74970648+catisioo@users.noreply.github.com>
2024-03-25 18:17:16 +0200

    GUI fails

--- file_for_testing_stuff.py
+++ file_for_testing_stuff.py
@@ -9,8 +9,14 @@ root.title("1. praktiskais darbs")
 currentNrLabel = tk.Label(root, text = "Current Number:", font = ('Cascadia Mono ExtraLight', 18))
 currentNrLabel.pack(padx = 20, pady = 40)
 
-div2button = tk.Button(root, text = "/2", font = ('Cascadia Mono ExtraLight', 10))
-div2button.pack(padx = 20, pady = 20)
+def open_about():
+    about_window = tk.Toplevel(root)
+    about_window.title("About authors")
+    label = tk.Label(about_window, text="Iļja Poļivko\nMārtiņš Ņikiforovs\nRaitis Bērtulis\nHaralds Popovs\nDaniela Alise Smelte\nViktors Krasnoborodjko", padx = 100, pady = 100)
+    label.pack()
+
+about_authors = tk.Button(root, text = "About authors", font = ('Cascadia Mono ExtraLight', 10), command=open_about)
+about_authors.place(relx=1, x=-5, y=5, anchor="ne")
 
 root.mainloop()
 
